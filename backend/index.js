1e60f2019caf22233f0c9f8348a08d515b44c2ac
Fey Vegborn <fey.vegborn@gmail.com>
2019-08-31 15:41:33 +0200

    servern hanterar taggar

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 
 const eventId = 'AB91BC33-D3E5-4746-9E98-EACA63421316';
 
+const tagList = [{"tag": "tag1","users": []}, {"tag": "Pop","users": []}, {"tag": "Country","users": []}, {"tag": "Classic","users": []}]
+
 const ws = new WebSocket(`wss://stagecast.se/api/events/${eventId}/ws`)
 
 ws.on('open', function open() {
@@ -17,9 +19,28 @@ ws.on('message', function incoming(data) {
     try {
        var data = JSON.parse(data.toString())
        console.log(JSON.stringify(data));
-    } catch(e) {
+       if (data.type === 'get-tags'){
+        ws.send(JSON.stringify({type:'tagslist', tagList}));
+       }
+       if (data.type === 'add-tag'){
+        if(-1 === tagList.indexOf(x => x.tag === data.tag)){
+             tagList.push({tag:data.tag, users:[data.user]})              
+        }
+        }
+       if (data.type === 'select-tag'){
+           if(-1 === tagList.find(x => x.tag === data.tag).users.indexOf(o => o === data.user)){
+                tagList.find(x => x.tag === data.tag).users.push(data.user);                  
+           }            
+       }
+       if (data.type === 'deselect-tag'){
+        tagList.find(x => x.tag === data.tag).users.pop(data.user);   
+       }               
+      ws.send(JSON.stringify({type:'tagslist', tagList}));    
+    }
+    catch(e) {
     }
 });
+
 
 var beat = 0;
 setInterval(() => {
